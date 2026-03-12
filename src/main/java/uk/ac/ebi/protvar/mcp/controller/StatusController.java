74a482795671d928aee3fabf3c0dde0b368d9660
prabhat <prabhat@ebi.ac.uk>
2026-03-12 19:49:07 +0000

    Changed the controller mapping from "/" to "/mcp"

--- src/main/java/uk/ac/ebi/protvar/mcp/controller/StatusController.java
+++ src/main/java/uk/ac/ebi/protvar/mcp/controller/StatusController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StatusController {
 
-    @GetMapping("/")
+    @GetMapping("/mcp")
     public String home() {
         return "ProtVar MCP Server is running";
     }
