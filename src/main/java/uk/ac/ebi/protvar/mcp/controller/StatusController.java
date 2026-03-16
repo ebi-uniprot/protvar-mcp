993aae22f48363d128d644986b32940e7b38cebd
prabhat <prabhat@ebi.ac.uk>
2026-03-16 13:24:02 +0000

    changed status endpoint

--- src/main/java/uk/ac/ebi/protvar/mcp/controller/StatusController.java
+++ src/main/java/uk/ac/ebi/protvar/mcp/controller/StatusController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StatusController {
 
-    @GetMapping("/status")
+    @GetMapping("/mcp/status")
     public String home() {
         return "ProtVar MCP Server is running";
     }
